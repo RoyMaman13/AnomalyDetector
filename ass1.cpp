c9b0be86806dff81edb2d8534316bdbc785bbb95
YonatanLahav <jonathanla180@gmail.com>
2021-10-16 07:27:29 -0400

    Finished var function+Test

--- ass1.cpp
+++ ass1.cpp
@@ -3,15 +3,17 @@
 
 float var(float *x, int size)
 {
+    if (size == 0)
+        return 0;
     float miu = 0, var = 0;
     for (int i = 1; i <= size; i++)
     {
         miu += x[i - 1];
     }
     miu /= size;
-    for (int i = 0; i <= size; i++)
+    for (int i = 1; i <= size; i++)
     {
         var += pow(x[i - 1] - miu, 2);
     }
-    return var/size;
+    return var / size;
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-
+#include "ass1.hpp"
 int main(int argc, char const *argv[])
 {
-    /* code */
+    float test[] = {1, 2, 3, 4, 5, 6};
+    std::cout << var(test, 6);
     return 0;
 }
