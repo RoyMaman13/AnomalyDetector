6ed46481a789504cbbb7b5b193be663f98a83ec2
YonatanLahav <jonathanla180@gmail.com>
2021-10-16 10:34:35 -0400

    Done.

--- anomaly_detection_util2.cpp
+++ anomaly_detection_util2.cpp
@@ -7,11 +7,6 @@
 #include <math.h>
 #include "anomaly_detection_util.h"
 
-float avg(float *x, int size)
-{
-	return 0;
-}
-
 // returns the variance of X and Y
 float var(float *x, int size)
 {
@@ -30,33 +25,22 @@ float var(float *x, int size)
 	return var / size;
 }
 
-// returns the covariance of X and Y
-float cov(float *x, float *y, int size)
-{
-	return 0;
-}
-
 // returns the Pearson correlation coefficient of X and Y
 float pearson(float *x, float *y, int size)
 {
-	return 0;
-}
-
-// performs a linear regression and returns the line equation
-Line linear_reg(Point **points, int size)
-{
-
-	return Line(0, 0);
+	if (sqrt(var(x, size)) == 0 || sqrt(var(y, size) == 0))
+		return 0;
+	return cov(x, y, size) / (sqrt(var(x, size)) * sqrt(var(y, size)));
 }
 
 // returns the deviation between point p and the line equation of the points
 float dev(Point p, Point **points, int size)
 {
-	return 0;
+	return dev(p,linear_reg(points,size));
 }
 
 // returns the deviation between point p and the line
 float dev(Point p, Line l)
 {
-	return 0;
+	return abs(l.f(p.x)-p.y);
 }
