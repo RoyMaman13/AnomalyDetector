c8ef8346db726900c1a0fb382cde90cb2d409393
YonatanLahav <jonathanla180@gmail.com>
2021-10-16 15:10:36 -0400

    Try with Clion

---
+++ cmake-build-debug/CMakeFiles/ass1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass1.dir/anomaly_detection_util.cpp.o"
+  "CMakeFiles/ass1.dir/main.cpp.o"
+  "ass1"
+  "ass1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ bool wrong(float val, float expected){
 
 // this is a simple test to put you on the right track
 int main(){
-	const int N=10;
+    const int N=10;
 	float x[]={1,2,3,4,5,6,7,8,9,10};
 	float y[]={2.1,4.2,6.1,8.1,10.3,12.2,14.4,16.1,18.2,20.3};
 
