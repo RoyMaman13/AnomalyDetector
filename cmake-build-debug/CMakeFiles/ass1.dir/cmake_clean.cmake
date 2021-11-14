1c6f1bc83265fb20c01dbc150661d3531fa1575f
ubuntu <jonathanla180@gmail.com>
2021-11-14 10:21:44 +0200

    Fixed all memory leaks via Valgrind. Need to add code comments.

---
+++ cmake-build-debug/CMakeFiles/ass1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass1.dir/MainTrain.cpp.o"
+  "CMakeFiles/ass1.dir/SimpleAnomalyDetector.cpp.o"
+  "CMakeFiles/ass1.dir/anomaly_detection_util.cpp.o"
+  "CMakeFiles/ass1.dir/timeseries.cpp.o"
+  "ass1"
+  "ass1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
